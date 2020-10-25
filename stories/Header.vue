d9763af572fbd62c41cfe7ea82f9f8f4b5cf5727
Prateek Obireddy <prateek@year13.com.au>
2020-10-26 09:46:21 +1100

    Safety commit

--- stories/Header.vue
+++ stories/Header.vue
@@ -21,9 +21,9 @@
         <h1>Acme</h1>
       </div>
       <div>
-        <my-button size="small" @onClick="onLogout" label="Log out" v-if="user" />
-        <my-button size="small" @onClick="onLogin" label="Log in" v-if="!user" />
-        <my-button primary size="small" @onClick="onCreateAccount" label="Sign up" v-if="!user" />
+        <my-button size="small" @onClick="onLogout" label="Log out" v-if="user"/>
+        <my-button size="small" @onClick="onLogin" label="Log in" v-if="!user"/>
+        <my-button primary size="small" @onClick="onCreateAccount" label="Sign up" v-if="!user"/>
       </div>
     </div>
   </header>
@@ -36,7 +36,7 @@ import MyButton from './Button.vue';
 export default {
   name: 'my-header',
 
-  components: { MyButton },
+  components: {MyButton},
 
   props: {
     user: {
